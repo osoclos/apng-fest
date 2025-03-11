c1ea50c3e781f5a1e750809173e7848491210664
osoclos <osoclosus@gmail.com>
2025-03-11 21:56:48 +0800

    fixed vite config package export

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,27 @@
 /// <reference types="vitest" />
 
+import fs from "fs/promises";
 import path from "path";
 
-import { defineConfig, type Plugin } from "vite";
+import { defineConfig } from "vite";
 import dtsPlugin from "vite-plugin-dts";
 
-import type { ModuleFormat } from "rollup";
+import type { ModuleFormat, OutputOptions  } from "rollup";
 import terser from "@rollup/plugin-terser";
 
-const TEST_SITE_FOLDER_PATH: string = path.join(__dirname, "tests/client");
-
-const stopAfterBuildPlugin = (): Plugin => {
-    return {
-        name: "stop-after-build",
-        buildEnd: () => process.exit()
-    }
-}
-
-const output = (<ModuleFormat[]>["es", "iife"]).map((format) => ({
+const output = <OutputOptions[]>(<ModuleFormat[]>["es", "iife"]).map((format) => ({
     format,
     
     entryFileNames: `index${format === "iife" ? "" : ".[format]"}.min.js`,
+    
     name: "APNG",
+    exports: "named",
 
     plugins: [terser()]
 }));
 
 export default defineConfig({
-    root: TEST_SITE_FOLDER_PATH,
+    root: "tests/client",
     resolve: { alias: { "apng-fest": path.join(__dirname, "src") } },
 
     test: { include: ["../unit/**/*.ts"] },
@@ -35,8 +29,10 @@ export default defineConfig({
         outDir: path.join(__dirname, "dist"),
         emitAssets: false,
 
+        emptyOutDir: true,
+
         sourcemap: true,
-        rollupOptions: { output, external: path.join(__dirname, "tests"),  },
+        rollupOptions: { output, external: path.join(__dirname, "tests") },
 
         lib: { entry: path.join(__dirname, "src/index.ts") }
     },
@@ -45,6 +41,10 @@ export default defineConfig({
         root: __dirname,
         tsconfigPath: "tsconfig.lib.json",
 
-        rollupTypes: true
-    }), stopAfterBuildPlugin()]
+        rollupTypes: true,
+        async afterBuild() {
+            const files = await fs.readdir(path.join(__dirname, "tests/client/public"));
+            for (const file of files) await fs.unlink(path.join(__dirname, "dist", file));
+        }
+    })]
 });
